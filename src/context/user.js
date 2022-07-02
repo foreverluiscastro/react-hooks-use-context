4b39de44a4fb4778e52f3a1e5f140eed0de3f43d
Luis <foreverluiscastro@gmail.com>
2022-07-01 21:23:43 -0400

    completed.

---
+++ src/context/user.js
@@ -0,0 +1,20 @@
+// src/context/user.js
+import React, { useState } from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+// create a provider component
+function UserProvider({ children }) {
+//   const currentUser = {
+//     name: "Duane",
+//     interests: ["Coding", "Biking", "Words ending in 'ing'"],
+//   };
+    const [user, setUser] = useState(null);
+  // the value prop of the provider will be our context data
+  // this value will be available to child components of this provider
+//   return <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+}
+
+export { UserContext, UserProvider };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "./context/theme";
 import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ThemeProvider><App /></ThemeProvider>, document.getElementById("root"));
